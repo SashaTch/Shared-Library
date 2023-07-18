04311e59a1b3d5241b6962bbd9ec0f70801d0e04
Alexander Tcherniahovski <134253868+SashaTch@users.noreply.github.com>
2023-07-18 16:40:10 +0300

    Update bannerUtils.groovy

--- vars/bannerUtils.groovy
+++ vars/bannerUtils.groovy
@@ -1,34 +1,10 @@
-def echoBanner(def ... msgs) {
-   echo createBanner(msgs)
+def echoMessage(msgs) {
+   echo "\e[92m${msgs}\e[0m\n"
 }
-
-def errorBanner(def ... msgs) {
-   error(createBanner(msgs))
-}
-
-def createBanner(def ... msgs) {
-   return """
-       ===========================================
-
-       ${msgFlatten(null, msgs).join("\n        ")}
-
-       ===========================================
-   """
-}
-
-// flatten function hack included in case Jenkins security
-// is set to preclude calling Groovy flatten() static method
-// NOTE: works well on all nested collections except a Map
-def msgFlatten(def list, def msgs) {
-   list = list ?: []
-   if (!(msgs instanceof String) && !(msgs instanceof GString)) {
-       msgs.each { msg ->
-           list = msgFlatten(list, msg)
-       }
-   }
-   else {
-       list += msgs
-   }
-
-   return  list
+def echoStage(msgs) {
+    echo """
+\e[35m==================================\e[0m
+\e[35m${msgs.center(46)}\e[0m
+\e[35m==================================\e[0m
+"""
 }
