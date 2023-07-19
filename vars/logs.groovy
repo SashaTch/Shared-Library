936828704c57dfa6b8d82c3eb0aec8600fe7fdb1
Alexander Tcherniahovski <134253868+SashaTch@users.noreply.github.com>
2023-07-19 15:49:59 +0300

    Update logs.groovy

--- vars/logs.groovy
+++ vars/logs.groovy
@@ -1,11 +1,11 @@
 //used for sup stages
 def echoGreen(msgs) {
-     echo "\033[32m|=====  ${msgs}  =====|\033[0m"
+     echo "\033[32m[=====<  ${msgs}  >====]|\033[0m"
 }
 
 //used for comments
 def echoYellow(msgs) {
-     echo "\033[33m${msgs}\033[0m"
+     echo "\033[33m<${msgs}>\033[0m"
 }
 
 //used for stages
