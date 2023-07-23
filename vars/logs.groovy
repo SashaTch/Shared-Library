24623761c02bf1013c47f16ffcd91cd0de0997d7
Alexander Tcherniahovski <134253868+SashaTch@users.noreply.github.com>
2023-07-23 14:08:20 +0300

    Update logs.groovy

--- vars/logs.groovy
+++ vars/logs.groovy
@@ -19,21 +19,23 @@ def echoStage(msgs) {
 """
 }
 
-def aws_credentials(shell_command) {
-     withCredentials([[
-                        $class: 'AmazonWebServicesCredentialsBinding',
-                        credentialsId: "AWS",
-                        accessKeyVariable: 'AWS_ACCESS_KEY_ID',
-                        secretKeyVariable: 'AWS_SECRET_ACCESS_KEY'
-                    ]]) {
-          // Split the shell command into separate commands
-          def commands = shell_command.split('$$')
-
-          // Iterate over each command
-          for (command in commands) {
-          // Trim and execute each command
-          command = command.trim()
-          sh "${command}"
-          echo "\033[33m<${command} has been completed successfully>\033[0m"
-     }
+def aws_credentials(shell_commands) {
+    withCredentials([[
+        $class: 'AmazonWebServicesCredentialsBinding',
+        credentialsId: "AWS",
+        accessKeyVariable: 'AWS_ACCESS_KEY_ID',
+        secretKeyVariable: 'AWS_SECRET_ACCESS_KEY'
+    ]]) {
+        // Split the shell commands into separate commands
+        def commands = shell_commands.split('\n')
+        // Iterate over each command
+        for (command in commands) {
+            // Trim and execute each command
+            command = command.trim()
+            if (command) { 
+                sh "${command}"
+                echo "\033[33m< excuting :${command} >\033[0m"
+            }
+        }
+    }
 }
