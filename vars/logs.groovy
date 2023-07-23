6ddf00f8f065a518051ec7319f08d91fa04dd0a8
Alexander Tcherniahovski <134253868+SashaTch@users.noreply.github.com>
2023-07-23 13:51:39 +0300

    Update logs.groovy

--- vars/logs.groovy
+++ vars/logs.groovy
@@ -18,3 +18,13 @@ def echoStage(msgs) {
 \033[35m|==========================================|\033[0m\n
 """
 }
+
+def aws_credentials(shell_command) {
+     withCredentials([[
+                        $class: 'AmazonWebServicesCredentialsBinding',
+                        credentialsId: "AWS",
+                        accessKeyVariable: 'AWS_ACCESS_KEY_ID',
+                        secretKeyVariable: 'AWS_SECRET_ACCESS_KEY'
+                    ]]) {
+          sh '${shell_command}'
+}
